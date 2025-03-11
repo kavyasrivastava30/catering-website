d263b416d48d9475a9a975d0e25456930b231171
kavyasrivastava30 <srivastavakavya2004@gmail.com>
2025-03-11 11:34:27 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 130,
+    img: '/catering website/biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 150,
+    img: '/catering website/project - Copy/chicken-hyderabadi-biryani-01.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 180,
+    img: '/catering website/project - Copy/egg-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 150,
+    img: '/catering website/project - Copy/chicken-biryani-benefits-main.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 100,
+    img: '/catering website/project - Copy/chickenbiryanibowltop.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 120,
+    img: '/catering website/project - Copy/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 130,
+    img: '/catering website/biryani.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 200,
+    img: '/catering website/project - Copy/memoni biryani.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 150,
+    img: '/catering website/project - Copy/mughlai biryani.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 110,
+    img: '/catering website/project - Copy/chicken roast.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 100,
+    img: '/catering website/project - Copy/chicken curry.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 140,
+    img: '/catering website/project - Copy/chicken do pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 120,
+    img: '/catering website/project - Copy/chicken masala.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 170,
+    img: '/catering website/project - Copy/handi chicken.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 200,
+    img: '/catering website/project - Copy/murgh musallam.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 150,
+    img: '/catering website/project - Copy/matar paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 100,
+    img: '/catering website/project - Copy/palak panner.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 150,
+    img: '/catering website/project - Copy/panner butter masala.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 120,
+    img: '/catering website/project - Copy/paneer do pyaza.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 80,
+    img: '/catering website/project - Copy/hydabadi paneer.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.3,
+    price: 70,
+    img: '/catering website/project - Copy/paneer lababdar.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 50,
+    img: '/catering website/project - Copy/paneer lababdar.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 80,
+    img: '/catering website/project - Copy/navratan korma.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 70,
+    img: '/catering website/project - Copy/veg jalfrezi.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 50,
+    img: '/catering website/biryani.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 70,
+    img: '/catering website/project - Copy/vey curry.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 100,
+    img: '/catering website/project - Copy/Veg-Kolhapuri-.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 40,
+    img: '/catering website/project - Copy/veg masala.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 40,
+    img: '/catering website/project - Copy/vey pakora.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 80,
+    img: '/catering website/project - Copy/momos.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 70,
+    img: '/catering website/project - Copy/chicken manchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 50,
+    img: '/catering website/project - Copy/chili chicken.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 160,
+    img: '/catering website/project - Copy/Chilli-Garlic-Noodles-2-3.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 140,
+    img: '/catering website/project - Copy/spring roll.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 100,
+    img: '/catering website/project - Copy/szechuan chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 80,
+    img: '/catering website/project - Copy/fried rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 180,
+    img: '/catering website/project - Copy/butter masala dosa.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 200,
+    img: '/catering website/project - Copy/idli.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 120,
+    img: '/catering website/project - Copy/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 100,
+    img: '/catering website/project - Copy/mysore bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 150,
+    img: '/catering website/project - Copy/onion uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 400,
+    img: '/catering website/project - Copy/plain dosa.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'RavaUttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 250,
+    img: '/catering website/project - Copy/rava uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 340,
+    img: '/catering website/project - Copy/Sambar-vada.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem}; 
